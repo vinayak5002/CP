a1dcbb3230bec4eb2bf5574af8afb389af26127a
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2022-08-26 11:12:19 +0530

    Linked list

---
+++ Linked List/Template.cpp
@@ -0,0 +1,16 @@
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
