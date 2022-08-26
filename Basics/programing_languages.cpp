f00859924e1281dcf1d14c9ccf80af1bf59f48cd
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2022-08-26 21:25:58 +0530

    programing lang

---
+++ Basics/programing_languages.cpp
@@ -0,0 +1,29 @@
+/*
+https://www.codechef.com/LP1TO201/problems/PROGLANG
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	   int a, b, a1, b1, a2, b2;
+	   cin>>a>>b>>a1>>b1>>a2>>b2;
+	   
+	   if((a==a1 || a==b1) && (b==a1 || b==b1)){
+	       cout<<"1"<<endl;
+	   }
+	   else if((a==a2 || a==b2) && (b==a2 || b==b2)){
+	       cout<<"2"<<endl;
+	   }
+	   else{
+	       cout<<"0"<<endl;
+	   }
+	    
+	}
+	return 0;
+}
