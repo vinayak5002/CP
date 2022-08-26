f4dc5668c7b2079e9a8bbf0a3760a877fd4c3d0a
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2022-08-26 21:24:54 +0530

    hoops

---
+++ Basics/hoops.cpp
@@ -0,0 +1,20 @@
+/*
+https://www.codechef.com/LP1TO201/problems/HOOPS
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int n;
+	    cin>>n;
+	    
+	    cout<<n/2+1<<endl;
+	}
+	return 0;
+}
